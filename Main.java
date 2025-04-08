ae4cec000279867b2edf83dbee6513cf73f0e12d
Rociodv1103 <rociodv1103@gmail.com>
2025-04-08 11:42:11 -0600

    RETO 1 S1 M1

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Paciente paciente1=new Paciente();
+        paciente1.nombre="Lena Marie";
+        paciente1.edad=22;
+        paciente1.expediente="EXP202409";
+        paciente1.mostrarInformacion();
+    }
+}
